cbe79e9abfde4567989239af1a7c2a4f5450cc8a
Thomas Bazire <thomas.bazire@me.com>
2016-02-07 00:20:40 +0100

    push something

---
+++ coloriss.js
@@ -0,0 +1,47 @@
+
+(function () {
+
+	this.Coloriss = (function () {
+
+		function Coloriss (color) {
+			this.color = color;
+		};
+		
+		Coloriss.prototype.hex = function (dash) {
+			var hex = dash ? '#' : '';
+			// 
+		};
+
+		Coloriss.prototype.rgb = function () {
+			var a, r, g, b;
+
+			this.alpha = a = this.color >> 24;
+			this.red = r = 0xFF & (this.color >> 16);
+			this.green = g = 0xFF & (this.color >> 8);
+			this.blue = b = 0xFF & this.color;
+			return {a:a, r:r, g:g, b:b};
+		};
+
+		Coloriss.prototype.red = function () {
+			return this.red;
+		}
+
+		Coloriss.prototype.grey = function () {
+			return (this.red + this.green + this.blue) / 3;
+		};
+
+		var toHex = function (val) {
+			n = parseInt(val, 10);
+			
+			if (isNaN(n)) return null;
+			
+			n = Math.max(0, Math.min(n, 255));
+			return "0123456789ABCDEF".charAt((n - n % 16) / 16)
+				+ "0123456789ABCDEF".charAt(n % 16);
+		}
+
+		return Coloriss;
+
+	})();
+
+})();
